88fde6f85532246bd4d0f24a709bc9e1be65bf33
Maksim <Maksim@CRUSTAL>
2024-11-21 23:50:49 +0300

    Добавьте файлы проекта.

---
+++ Пр8/Interface1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пр8
+{
+    interface IShip
+    { 
+        int Length { get;}
+        string Name { get; set; }
+        int Width { get;}
+        int Height { get;}
+        string GetInfo();
+    }
+
+    interface ICargoTransport
+    {
+        int Length { get; }
+        string Name { get; set; }
+        int Width { get; }
+        int Height { get; }
+        int LiftingCapacity { get;}
+        string GetInfo();
+    }
+
+    public class CargoShip : IComparable<CargoShip>, IShip, ICargoTransport, ICloneable
+    {
+        public int Length { get;} 
+        public string Name { get; set; }
+        public int Width { get;}
+        public int Height { get;}
+        public int LiftingCapacity { get;}
+        public int CompareTo(CargoShip other)
+        {
+            if (other == null) return 1;
+            return LiftingCapacity.CompareTo(other.LiftingCapacity);
+        }
+
+        public CargoShip (int length, int width, int height, string name, int lifting)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+            Name = name;
+            LiftingCapacity = lifting;
+        }
+
+        public string GetInfo()
+        {
+            return $"Корабль: {Name}, Грузоподъемность: {LiftingCapacity} тонн, Высота: {Height} метров, Длина: {Length} метров, Ширина: {Height} метров.";
+        }
+
+        public object Clone()
+        {
+            return new CargoShip(this.Length, this.Width, this.Height, this.Name, this.LiftingCapacity );
+        }
+    }
+}
---
+++ Пр8/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Пр8;
+
+namespace Пр8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CargoShip ship1;
+        private CargoShip ship2;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ship1 = new CargoShip(100,50,30,"1",400);
+            ship2 = (CargoShip)ship1.Clone();
+            ship2.Name = "2";
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: Кузнецов М.Н. ИСП-31 \nСоздать интерфейсы - корабль, грузовой транспорт. Создать класс грузовой корабль. Класс должен включать конструктор, функцию для формирования строки информации о корабле. Сравнение производить по грузоподъемности.");
+        }
+
+        private void btnOutput1_Click(object sender, RoutedEventArgs e)
+        {
+            Ship1.Text = ship1.GetInfo();
+        }
+
+        private void btnOutput2_Click(object sender, RoutedEventArgs e)
+        {
+            Ship2.Text = ship2.GetInfo();
+        }
+
+        private void btnCompare_Click(object sender, RoutedEventArgs e)
+        {
+            if (ship1.CompareTo(ship2) < 0)
+            {
+                Rez.Text=$"{ship1.Name} менее грузоподъемен, чем {ship2.Name}";
+            }
+            else if (ship1.CompareTo(ship2) > 0)
+            {
+                Rez.Text = $"{ship1.Name} более грузоподъемен, чем {ship2.Name}";
+            }
+            else
+            {
+                Rez.Text = $"{ship1.Name} и {ship2.Name} имеют одинаковую грузоподъемность";
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this .Close();
+        }
+    }
+}
